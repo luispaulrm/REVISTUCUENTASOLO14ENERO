f04a1f5b612e77b87459e5d048c8489ee8d7685c
luispaulrm <lpaulr@gmail.com>
2025-12-31 16:28:29 -0300

    chore: bump version to v1.5.8 and update last modified timestamp

--- version.ts
+++ version.ts
@@ -1,2 +1,2 @@
-export const VERSION = "v1.5.7";
-export const LAST_MODIFIED = "31/12/2025 16:25 (Chile)";
+export const VERSION = "v1.5.8";
+export const LAST_MODIFIED = "31/12/2025 16:28 (Chile)";
