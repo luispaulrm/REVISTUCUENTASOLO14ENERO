9b0a55b99c6fb1dd3088cddd318e70ed3b060d5d
luispaulrm <lpaulr@gmail.com>
2025-12-31 15:00:14 -0300

    chore: Update version to v1.4.3 and last modified timestamp.

--- version.ts
+++ version.ts
@@ -1,2 +1,2 @@
-export const VERSION = "v1.4.2";
-export const LAST_MODIFIED = "31/12/2025 12:35 (Chile)";
+export const VERSION = "v1.4.3";
+export const LAST_MODIFIED = "31/12/2025 15:00 (Chile)";
