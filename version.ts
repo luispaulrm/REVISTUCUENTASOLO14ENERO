9119c481480c23a281cbc8cd784a06be84bb77ad
luispaulrm <lpaulr@gmail.com>
2026-01-01 22:45:10 -0300

    feat: add item counter to AuditSummary for fiscalization and increment version

--- version.ts
+++ version.ts
@@ -1,2 +1,2 @@
-export const VERSION = "v1.5.19";
-export const LAST_MODIFIED = "01/01/2026 22:33 (Chile)";
+export const VERSION = "v1.5.20";
+export const LAST_MODIFIED = "01/01/2026 22:45 (Chile)";
