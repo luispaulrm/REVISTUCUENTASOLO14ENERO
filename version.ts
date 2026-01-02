83cb953d34fecf373a93f9d6e0d6f306661f7870
luispaulrm <lpaulr@gmail.com>
2026-01-01 23:11:50 -0300

    chore: jump to v1.6.0 to force refresh and fix cache issues

--- version.ts
+++ version.ts
@@ -1,3 +1,3 @@
-export const VERSION = "v1.5.27";
-export const LAST_MODIFIED = "01/01/2026 23:08 (Chile)";
+export const VERSION = "v1.6.0";
+export const LAST_MODIFIED = "01/01/2026 23:12 (Chile)";
 export const AI_MODEL = "Gemini 3 Pro Preview";
