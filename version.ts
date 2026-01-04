030ab2d55e217f524e43e92814465eef5a3b6586
luispaulrm <lpaulr@gmail.com>
2026-01-04 19:13:14 -0300

    chore: fix version time v1.9.208

--- version.ts
+++ version.ts
@@ -1,3 +1,3 @@
-export const VERSION = "v1.9.207";
-export const LAST_MODIFIED = "04/01/2026 19:25 (Chile)";
+export const VERSION = "v1.9.208";
+export const LAST_MODIFIED = "04/01/2026 19:13 (Chile)";
 export const AI_MODEL = "Gemini 3 Strict (All Mod)";
