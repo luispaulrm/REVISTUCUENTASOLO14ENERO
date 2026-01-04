5e9db58e2d0ec3aa2c7e6cab0d7637364712ec22
luispaulrm <lpaulr@gmail.com>
2026-01-04 16:13:18 -0300

    chore: bump version to v1.9.192 [skip ci]

--- version.ts
+++ version.ts
@@ -1,3 +1,3 @@
-export const VERSION = "v1.9.191";
-export const LAST_MODIFIED = "04/01/2026 16:10 (Chile)";
+export const VERSION = "v1.9.192";
+export const LAST_MODIFIED = "04/01/2026 16:13 (Chile)";
 export const AI_MODEL = "Gemini 3 Flash";
