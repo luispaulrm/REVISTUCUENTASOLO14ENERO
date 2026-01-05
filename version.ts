e43cdbf5c4b2ab4f60ff41c1561aeed056d65d8e
luispaulrm <lpaulr@gmail.com>
2026-01-05 09:51:17 -0300

    chore: bump version to v1.9.225 [skip ci]

--- version.ts
+++ version.ts
@@ -1,3 +1,3 @@
-export const VERSION = "v1.9.224";
-export const LAST_MODIFIED = "04/01/2026 21:58 (Chile)";
+export const VERSION = "v1.9.225";
+export const LAST_MODIFIED = "05/01/2026 09:51 (Chile)";
 export const AI_MODEL = "Gemini 3 Universal v8.5 (Human Scanner)";
